6e14512c0661a96ee81ccbdef58eec23793d7cfd
Sergej <sergej.kravets.90@gmail.com>
2025-01-06 16:21:21 +0200

    Add some files

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+export default {
+  presets: [
+    '@babel/preset-env',    // Позволяет использовать последние версии JavaScript
+    '@babel/preset-react'   // Преобразует JSX в стандартный JavaScript
+  ]
+};
